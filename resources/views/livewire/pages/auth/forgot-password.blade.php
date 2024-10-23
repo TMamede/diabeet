6594214cf027066d7ed71b5b4f301a002d1966bb
TMamede <tamires.m.maia@gmail.com>
2024-10-23 16:52:24 -0300

    Esqueci Senha(2)

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -54,7 +54,7 @@ new #[Layout('layouts.guest')] class extends Component
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Redefinir Senha') }}
             </x-primary-button>
         </div>
     </form>
