08e56c80f6827b08248b73438ca66ed51891b816
sarahvit <sarahmalenaaquino54@gmail.com>
2025-12-03 16:19:39 -0300

    feat: termos de uso

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav x-data="{ open: false }" @keydown.escape.window="open = false" class="relative bg-gray-100 border-b border-gray-200">
     <!-- Barra principal -->
     <div class="px-4 mx-auto max-w-8xl sm:px-6 lg:px-12">
-        <div class="flex items-center justify-between h-20">
+        <div class="flex items-center justify-between h-20 ">
             <!-- ESQUERDA: logo + links desktop -->
             <div class="flex items-center gap-3">
                 <!-- Logo -->
@@ -12,7 +12,7 @@
                 </div>
 
                 <!-- Links desktop -->
-                <div class="hidden space-x-16 lg:-my-px lg:ms-12 lg:flex">
+                <div class="hidden space-x-10 lg:-my-px lg:ms-12 lg:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Início') }}
                     </x-nav-link>
@@ -35,6 +35,9 @@
                     </x-nav-link>
                     <x-nav-link :href="route('about')" :active="request()->routeIs('about')" wire:navigate>
                         {{ __('Sobre Nós') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('termos')" :active="request()->routeIs('termos')" wire:navigate>
+                        {{ __('Termos de uso') }}
                     </x-nav-link>
                     
                 </div>
@@ -129,6 +132,7 @@
                                         <span>{{ __('Mapa do site') }}</span>
                                     </div>
                                 </x-dropdown-link>
+                                
 
                                 <div class="my-2 border-t border-gray-100"></div>
 
